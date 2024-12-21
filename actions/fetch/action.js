77a9f48342c163f1fdc7bd9dab0b5a6d691ec04c
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-21 18:46:06 +0100

    Update action.js

--- actions/fetch/action.js
+++ actions/fetch/action.js
@@ -5,9 +5,9 @@ import createPRs from './create-prs.js';
 
 const url = core.getInput('url');
 const requireMetadata = core.getInput('require-metadata');
-const results = await fetch({
+const result = await fetch({
 	id: url,
 	requireMetadata,
 });
 
-await createPRs(results);
+await createPRs(result.packages);
