8eb6c7e05d8bd042ac33f4e12899e0cc350c7d73
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2024-12-22 20:57:00 +0100

    Properly log errors

--- actions/fetch/action.js
+++ actions/fetch/action.js
@@ -4,16 +4,22 @@ import fetch from './fetch.js';
 
 const url = core.getInput('url');
 const requireMetadata = core.getInput('require-metadata');
-const { packages, timestamp } = await fetch({
-	id: url,
-	requireMetadata,
-});
+try {
+	const { packages, timestamp } = await fetch({
+		id: url,
+		requireMetadata,
+	});
 
-// Set the proper output variables.
-let hasNewContent = packages.length > 0;
-core.setOutput('packages', JSON.stringify(packages));
-if (timestamp) core.setOutput('timestamp', timestamp);
-core.setOutput('has-new-content', hasNewContent);
-if (!hasNewContent) {
-	core.notice('No new content was found.');
+	// Set the proper output variables.
+	let hasNewContent = packages.length > 0;
+	core.setOutput('packages', JSON.stringify(packages));
+	if (timestamp) core.setOutput('timestamp', timestamp);
+	core.setOutput('has-new-content', hasNewContent);
+	if (!hasNewContent) {
+		core.notice('No new content was found.');
+	}
+
+} catch (e) {
+	core.error(e.message);
+	throw e;
 }
