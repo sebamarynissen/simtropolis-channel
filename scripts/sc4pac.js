8ad8eeefa16f98ee5154fe5f3bd127f4b8037318
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-15 18:43:34 +0100

    Don't install dependencies in actual plugins folder

--- scripts/sc4pac.js
+++ scripts/sc4pac.js
@@ -8,7 +8,6 @@ import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 import { parseAllDocuments } from 'yaml';
 import { Minimatch } from 'minimatch';
-import { styleText } from 'node:util';
 
 // Parse the regular expressions for the packages
 const { argv } = yargs(hideBin(process.argv));
@@ -33,17 +32,9 @@ const packages = glob
 	.flat()
 	.sort();
 
-const pluginsRoot = process.env.SC4_PLUGINS;
-if (fs.existsSync(pluginsRoot)) {
-	const dir = await fs.promises.readdir(pluginsRoot);
-	if (dir.length !== 0) {
-		console.error(styleText('red', 'Your plugins folder is not empty. Please empty it manually and then run this script again.'));
-		process.exit(1);
-	}
-}
-
 // Now generate the sc4pac-plugins.json file.
-const cacheRoot = process.env.SC4PAC_CACHE_FOLDER;
+const pluginsRoot = path.resolve(import.meta.dirname, '../dist/plugins');
+const cacheRoot = process.env.SC4PAC_CACHE_ROOT;
 const json = {
 	config: {
 		pluginsRoot,
@@ -58,16 +49,14 @@ const json = {
 	explicit: packages,
 };
 
-const dist = path.resolve(import.meta.dirname, '../dist/plugins');
-await fs.promises.rm(path.join(dist, 'sc4pac-plugins-lock.json'), { force: true, recursive: true });
-await fs.promises.mkdir(dist, { recursive: true });
+await fs.promises.mkdir(pluginsRoot, { recursive: true });
 await fs.promises.writeFile(
-	path.join(dist, 'sc4pac-plugins.json'),
+	path.join(pluginsRoot, 'sc4pac-plugins.json'),
 	JSON.stringify(json, null, 2),
 );
 
 // Now run sc4pac.
 cp.execSync('sc4pac update -y', {
-	cwd: dist,
+	cwd: pluginsRoot,
 	stdio: 'inherit',
 });
