37e5f8139d88bcf118b0683fc7c7cb612f836e05
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-23 13:57:30 +0100

    Refactor listing dependencies

--- scripts/list-dependencies.js
+++ scripts/list-dependencies.js
@@ -5,120 +5,151 @@ import ora from 'ora';
 import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs/yargs';
 
-const channels = [
-	String(new URL('../dist/channel/', import.meta.url)),
-	'https://memo33.github.io/sc4pac/channel/',
-	'https://sc4pac.simtropolis.com/',
-];
+class DependencyLister {
+	index = {};
+	cache = {};
+	channels = [
+		String(new URL('../dist/channel/', import.meta.url)),
+		'https://memo33.github.io/sc4pac/channel/',
+		'https://sc4pac.simtropolis.com/',
+	];
 
-async function buildIndex() {
-	let spinner = ora('Building up package index').start();
-	let index = {};
-	await Promise.all(channels.map(url => buildChannelIndex(url, index)));
-	spinner.succeed();
-	return index;
-}
-
-async function getJSON(url) {
-	let parsed = new URL(url);
-	if (parsed.protocol === 'file:') {
-		let contents = await fs.promises.readFile(parsed);
-		return JSON.parse(String(contents));
-	} else {
-		let res = await fetch(url);
-		return await res.json();
+	// ## buildIndex()
+	// Fetches the package index from all the configured channgels.
+	async buildIndex() {
+		let spinner = ora('Building up package index').start();
+		await Promise.all(this.channels.map(url => this.buildChannelIndex(url)));
+		spinner.succeed();
 	}
-}
 
-async function buildChannelIndex(channel, index = {}) {
-	let url = new URL('./sc4pac-channel-contents.json', channel);
-	let json = await getJSON(url);
-	for (let pkg of json.packages) {
-		let id = `${pkg.group}:${pkg.name}`;
-		let arr = index[id] ??= [];
-		arr.push({
-			...pkg,
-			channel,
-		});
-		arr.sort((a, b) => {
-			return channels.indexOf(a.channel) - channels.indexOf(b.channel);
-		});
-	}
-	return index;
-}
-
-async function getInfo(pkg) {
-	let { group, name, channel } = pkg;
-	let url = new URL(`./metadata/${group}/${name}/latest/pkg.json`, channel);
-	return await getJSON(url);
-}
-
-async function getDependencies(pkg, index) {
-	let def = index[pkg];
-	if (!def) throw new Error(`Package ${pkg} not found in any of the channels.`);
-	let info = await getInfo(def[0]);
-	let html = '<div>';
-	let common = new Set(
-		info.variants
-			.map(variant => variant.dependencies ?? [])
-			.flat()
-			.map(pkg => `${pkg.group}:${pkg.name}`),
-	);
-	for (let variant of info.variants) {
-		let { dependencies = [] } = variant;
-		let subset = dependencies.map(pkg => `${pkg.group}:${pkg.name}`);
-		common = common.intersection(new Set(subset));
-	}
-	html += '<h4>Dependencies:</h4>';
-	html += await generateList([...common].sort());
-	for (let variant of info.variants) {
-		let { dependencies = [] } = variant;
-		let subset = new Set(dependencies.map(pkg => `${pkg.group}:${pkg.name}`));
-		let unique = subset.difference(common);
-		if (unique.size === 0) continue;
-		let title = Object.entries(variant.variant)
-			.map(arr => arr.join(': '))
-			.join('? ');
-		if (title === 'nightmode: dark') {
-			title = 'Darknite version only';
-		}
-		html += `<h4>${title}:</h4>`;
-		html += await generateList([...unique].sort());
-	}
-	html += '</div>';
-	html += '<div><button>Copy</button></div>';
-	html += `<script>document.querySelector('button').addEventListener('click', async () => {
-		let html = document.querySelector('div').outerHTML;
-		let text = [...document.querySelectorAll('div > ul > li a')].map(a => {
-			return '- '+a.textContent.trim();
-		}).join('\\n');
-		await navigator.clipboard.write([new ClipboardItem({
-			'text/html': new Blob([html], { type: 'text/html' }),
-			'text/plain': new Blob([text], { type: 'text/plain' }),
-		})]);
-	});</script>`;
-	let url = new URL('../dist/copy.html', import.meta.url);
-	await fs.promises.writeFile(url, html);
-	console.log(`Written output to ${url}`);
-}
-
-async function generateList(dependencies) {
-	let html = '<ul>';
-	for (let pkg of dependencies) {
-		let def = index[pkg];
-		let metadata = await getInfo(def[0]);
-		let {
-			website,
-			websites = website ? [website] : [],
-		} = metadata.info;
-		if (websites.length > 0) {
-			html += `<li><a href="${websites[0]}">${pkg}</a></li>\n`;
+	// ## getJSON(url)
+	// Gets the contents of a given url (might be a file url) as json.
+	async getJSON(url) {
+		let parsed = new URL(url);
+		if (parsed.protocol === 'file:') {
+			let contents = await fs.promises.readFile(parsed);
+			return JSON.parse(String(contents));
 		} else {
-			html += `<li>${pkg}</li>`;
+			let res = await fetch(url);
+			return await res.json();
 		}
 	}
-	html += '</ul>';
-	return html;
+
+	// ## buildChannelIndex(channel)
+	// Builds up the package index from a specific channel.
+	async buildChannelIndex(channel) {
+		const { channels } = this;
+		let url = new URL('./sc4pac-channel-contents.json', channel);
+		let json = await this.getJSON(url);
+		for (let pkg of json.packages) {
+			let id = `${pkg.group}:${pkg.name}`;
+			let arr = this.index[id] ??= [];
+			arr.push({
+				...pkg,
+				channel,
+			});
+			arr.sort((a, b) => {
+				return channels.indexOf(a.channel) - channels.indexOf(b.channel);
+			});
+		}
+	}
+
+	// ## getInfo(pkg)
+	// Fetches the full package info.
+	async getInfo(pkg) {
+		let { group, name, channel } = pkg;
+		let url = new URL(
+			`./metadata/${group}/${name}/latest/pkg.json`,
+			channel,
+		);
+		if (url in this.cache) {
+			return await this.cache[url];
+		} else {
+			let promise = this.getJSON(url);
+			this.cache[url] = promise;
+			return await promise;
+		}
+	}
+
+	// ## getDependencies(id)
+	// Generates a full html document containing all dependencies for the given 
+	// pkg.
+	async getDependencies(id) {
+		await this.buildIndex();
+		let packages = this.index[id];
+		if (!packages) {
+			throw new Error(`Package ${id} not found in any of the channels.`);
+		}
+		let pkg = await this.getInfo(packages[0]);
+		let html = `<style>* {font-family: ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";}</style>`;
+		html += `<div><h1>${pkg.info.summary}</h1>`;
+		if (pkg.info.images) {
+			html += `<img src="${pkg.info.images[0]}" style="width: 256px; height: 256px; object-fit: cover">`;
+		}
+		let common = new Set(
+			pkg.variants
+				.map(variant => variant.dependencies ?? [])
+				.flat()
+				.map(pkg => `${pkg.group}:${pkg.name}`),
+		);
+		for (let variant of pkg.variants) {
+			let { dependencies = [] } = variant;
+			let subset = dependencies.map(pkg => `${pkg.group}:${pkg.name}`);
+			common = common.intersection(new Set(subset));
+		}
+		html += '<h4>Dependencies:</h4>';
+		html += await this.generateList([...common].sort());
+		for (let variant of pkg.variants) {
+			let { dependencies = [] } = variant;
+			let subset = new Set(dependencies.map(pkg => `${pkg.group}:${pkg.name}`));
+			let unique = subset.difference(common);
+			if (unique.size === 0) continue;
+			let title = Object.entries(variant.variant)
+				.map(arr => arr.join(': '))
+				.join('? ');
+			if (title === 'nightmode: dark') {
+				title = 'Darknite version only';
+			}
+			html += `<h4>${title}:</h4>`;
+			html += await this.generateList([...unique].sort());
+		}
+		html += '</div>';
+		html += '<div><button>Copy dependencies</button></div>';
+		html += `<script>document.querySelector('button').addEventListener('click', async () => {
+			let html = document.querySelector('div').outerHTML;
+			let text = [...document.querySelectorAll('div > ul > li a')].map(a => {
+				return '- '+a.textContent.trim();
+			}).join('\\n');
+			await navigator.clipboard.write([new ClipboardItem({
+				'text/html': new Blob([html], { type: 'text/html' }),
+				'text/plain': new Blob([text], { type: 'text/plain' }),
+			})]);
+		});</script>`;
+		let url = new URL('../dist/copy.html', import.meta.url);
+		await fs.promises.writeFile(url, html);
+		console.log(`Written output to ${url}`);
+	}
+
+	// # generateList(deps)
+	async generateList(deps) {
+		let html = '<ul>';
+		for (let pkg of deps) {
+			let def = this.index[pkg];
+			let metadata = await this.getInfo(def[0]);
+			let {
+				website,
+				websites = website ? [website] : [],
+			} = metadata.info;
+			if (websites.length > 0) {
+				html += `<li><a href="${websites[0]}">${pkg}</a></li>\n`;
+			} else {
+				html += `<li>${pkg}</li>`;
+			}
+		}
+		html += '</ul>';
+		return html;
+	}
+
 }
 
 const { argv } = yargs(hideBin(process.argv));
@@ -126,5 +157,6 @@ if (argv._.length === 0) {
 	console.error(styleText('red', 'Please specify a package as argument'));
 	process.exit(1);
 }
-let index = await buildIndex();
-await getDependencies(argv._[0], index);
+
+const lister = new DependencyLister();
+await lister.getDependencies(argv._[0]);
