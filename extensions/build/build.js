702dcf3377ae632895201132cc7543967b7e3162
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-25 15:29:19 +0100

    feat: scaffold basic browser extension (#56)
    * Scaffold basic Chrome extension
    
    * Add icons
    
    * Copy authentication credentials to clipboard
    
    * Fix console logging
    
    * Rename `chrome` to `src`
    
    * Add esbuild
    
    * Refactor extension to provide `setup()` function
    
    * Add build script
    
    * Allow external ids to be specified
    
    * Add link for viewing package to STEX
    
    * Rename content script to `stex.js`
    
    * Allow setting sc4pac server
    
    * Improve styling
    
    * Shorten timeout
    
    * Port to Firefox
    
    * Implement pack script
    
    * Display `sc4pac://` install url
    
    * Refactor
    
    * Update setup.js
    
    * Use `sc4pac.simtropolis.com` as channel url
    
    * Implement proper sc4pac:/// protocol
    
    See https://github.com/memo33/sc4pac-gui/pull/17
    
    * Remove calling localhost
    
    It's no longer required to have the gui already running with the new sc4pac:/// protocol, so we've removed the code. The protocol experience is vastly superior.
    
    * Limit script to `/files/file` url
    
    * Minify script
    
    * Update stex.js
    
    * Handle duplicate package ids

---
+++ extensions/build/build.js
@@ -0,0 +1,70 @@
+// # build.js
+import path from 'node:path';
+import fs from 'node:fs';
+import { createRequire } from 'node:module';
+import * as esbuild from 'esbuild';
+import { Glob } from 'glob';
+const require = createRequire(import.meta.url);
+const manifest = require('../src/manifest.json');
+const srcDir = path.resolve(import.meta.dirname, '../src');
+const outDir = path.resolve(import.meta.dirname, '../dist');
+
+const files = [
+	'stex.js',
+	'background.js',
+	'copy.js',
+];
+const config = {
+	chrome: {
+		files,
+	},
+	firefox: {
+		files,
+		manifest: {
+			browser_specific_settings: {
+				gecko: {
+					id: 'browser-extension@sc4pac-tools',
+					strict_min_version: '109.0',
+				},
+			},
+			background: {
+				scripts: ['background.js'],
+			},
+		},
+	},
+};
+
+try {
+	await fs.promises.rm(outDir, { recursive: true });
+} catch {}
+
+for (let [browser, options] of Object.entries(config)) {
+	let outDir = path.resolve(import.meta.dirname, '../dist', browser);
+	let { files } = options;
+	for (let file of files) {
+		await esbuild.build({
+			entryPoints: [file],
+			absWorkingDir: srcDir,
+			bundle: true,
+			outfile: path.join(outDir, file),
+			minify: true,
+		});
+	}
+
+	await fs.promises.writeFile(
+		path.join(outDir, 'manifest.json'),
+		JSON.stringify({
+			...manifest,
+			...options.manifest,
+		}, null, 2),
+	);
+
+	let glob = new Glob('*.png', { cwd: srcDir });
+	for await (let file of glob) {
+		await fs.promises.copyFile(
+			path.join(srcDir, file),
+			path.join(outDir, file),
+		);
+	}
+
+}
