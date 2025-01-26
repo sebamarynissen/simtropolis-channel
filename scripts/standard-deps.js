d99eef6c3b39b1ae46f4155ee2502889b6cfc7ed
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-26 17:48:42 +0100

    Update standard-deps.js

--- scripts/standard-deps.js
+++ scripts/standard-deps.js
@@ -1,6 +1,9 @@
 // # standard-deps.js
 export default [
 	'memo:essential-fixes',
+	'memo:transparent-texture-fix-dll',
+	'memo:region-thumbnail-fix-dll',
+	'peg:oops-mod',
 	'simmaster07:sc4fix',
 	'simmaster07:extra-cheats-dll',
 ];
