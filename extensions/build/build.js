f9f248ba2a1bed19f71d7d0c57404af6db134521
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2025-01-26 22:07:05 +0100

    build: remove install button from extensions (#111)
    * Build extensions with stex script
    
    * Separately build stex script
    
    * Create zips when building

--- extensions/build/build.js
+++ extensions/build/build.js
@@ -1,16 +1,16 @@
 // # build.js
 import path from 'node:path';
 import fs from 'node:fs';
+import cp from 'node:child_process';
 import { createRequire } from 'node:module';
 import * as esbuild from 'esbuild';
 import { Glob } from 'glob';
 const require = createRequire(import.meta.url);
 const manifest = require('../src/manifest.json');
 const srcDir = path.resolve(import.meta.dirname, '../src');
-const outDir = path.resolve(import.meta.dirname, '../dist');
+const outDir = path.resolve(import.meta.dirname, '../../dist/extensions');
 
 const files = [
-	'stex.js',
 	'background.js',
 	'copy.js',
 ];
@@ -39,20 +39,20 @@ try {
 } catch {}
 
 for (let [browser, options] of Object.entries(config)) {
-	let outDir = path.resolve(import.meta.dirname, '../dist', browser);
+	let browserOutDir = path.join(outDir, browser);
 	let { files } = options;
 	for (let file of files) {
 		await esbuild.build({
 			entryPoints: [file],
 			absWorkingDir: srcDir,
 			bundle: true,
-			outfile: path.join(outDir, file),
+			outfile: path.join(browserOutDir, file),
 			minify: true,
 		});
 	}
 
 	await fs.promises.writeFile(
-		path.join(outDir, 'manifest.json'),
+		path.join(browserOutDir, 'manifest.json'),
 		JSON.stringify({
 			...manifest,
 			...options.manifest,
@@ -63,8 +63,25 @@ for (let [browser, options] of Object.entries(config)) {
 	for await (let file of glob) {
 		await fs.promises.copyFile(
 			path.join(srcDir, file),
-			path.join(outDir, file),
+			path.join(browserOutDir, file),
 		);
 	}
 
+}
+
+// Now that the STEX script is live, we have to build it separately.
+await esbuild.build({
+	entryPoints: ['stex.js'],
+	absWorkingDir: srcDir,
+	bundle: true,
+	outfile: path.join(outDir, 'scripts/stex.js'),
+	minify: true,
+});
+
+// Pack up as .zip.
+for (let browser of Object.keys(config)) {
+	cp.execSync(`7z a ../${browser}.zip *`, {
+		cwd: path.join(outDir, browser),
+		stdio: 'inherit',
+	});
 }
